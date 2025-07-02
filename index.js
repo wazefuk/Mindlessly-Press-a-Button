36be228ee5a2a8a9f982b950c060f21f8ddb90d8
Wazefuk <112293501+wazefuk@users.noreply.github.com>
2025-07-01 19:25:18 -0500

    MORE JAVASCRIPT (and CSS and HTML but whatever)

--- index.js
+++ index.js
@@ -1,8 +1,73 @@
-var Presses = 0;
+var Presses = 999999999;
+var PressesPerPress = 1;
+var AutoclickerDelay = 5000;
+var Gold = 0;
+var autoclickerBuy = false;
 
 const Button = document.getElementById("theButton");
+const Pressp = document.getElementById("pressp");
+const Goldp = document.getElementById("goldp");
+const Pressesperpressp = document.getElementById("pppp");
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === ' ' || event.key === 'Enter' || event.key === 'e') {
+        press();
+    }
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function updateUI() {
+    Button.style.rotate = Presses + "deg";
+    Pressp.innerText = Presses + " presses";
+    Goldp.innerText = Gold + " gold";
+    Pressesperpressp.innerHTML = PressesPerPress + " presses per press";
+}
 
 function press() {
-    Presses += 1;
-    Button.style.rotate = Presses + "deg";
+    Presses += PressesPerPress;
+    updateUI();
 }
+
+async function Autoclicker() {
+    while (true) {
+        await sleep(AutoclickerDelay);
+        press();
+    }
+}
+
+function buyUpgrade(input) {
+    switch (input) {
+        case 1:
+            if (Presses >= 360) {
+                Presses -= 360;
+                PressesPerPress++;
+                break;
+            }
+        case 2:
+            if (Presses >= 800) {
+                Presses -= 800;
+                PressesPerPress += 5;
+                break;
+            }
+        case 3:
+            if (Presses >= 3500 && autoclickerBuy == false) {
+                Presses -= 3500;
+                Autoclicker();
+                document.getElementById("upg-autoclicker").disabled = true;
+                autoclickerBuy = true;
+                break;
+            }
+        case 4:
+            if (Presses >= 2200 && AutoclickerDelay > 80) {
+                Presses -= 2200;
+                AutoclickerDelay -= 20;
+                break;
+            }
+    }
+    updateUI();
+}
+
+updateUI();
